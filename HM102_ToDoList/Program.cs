f71d426688314f5e6ea320e37ccbf2064005a240
zahrablend <zahra@LAPTOP-010BK67M>
2023-07-01 16:00:55 +0300

    Add project files.

---
+++ HM102_ToDoList/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HM102_ToDoList
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDoList toDoList = new ToDoList();
+            toDoList.ItemsToDo.Add(new Task { Title = "Read a book", Description = "Read chapter 7 of \"The Pragmatic programmer\"", DueDate = DateTime.Now.AddDays(1), IsDone = false });
+            toDoList.ItemsToDo.Add(new Task { Title = "Plant vegetables", Description = "Plant zucchini, tomatoe and bell-pepper plants", DueDate = DateTime.Now.AddDays(2), IsDone = true });
+            toDoList.ItemsToDo.Add(new Task { Title = "Vaccination appointment", Description = "Make appointment with a vet to vaccinate the cat", DueDate = DateTime.Now.AddDays(3), IsDone = false });
+            toDoList.ItemsToDo.Add(new Task { Title = "Meet friend", Description = "Call a friend and agree to meet", DueDate = DateTime.Now.AddDays(4), IsDone = true });
+            toDoList.ItemsToDo.Add(new Task { Title = "Collect berries", Description = "Collect raspberries and blueberies from the garden", DueDate = DateTime.Now.AddDays(5), IsDone = false });
+
+            toDoList.FindNotDoneItems();
+            toDoList.FindNextWeekItems();
+        }
+    }
+}
---
+++ HM102_ToDoList/ToDoList.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Linq;
+
+namespace HM102_ToDoList
+{
+    public class ToDoList
+    {
+        public List<Task> ItemsToDo { get; set; }
+
+        public ToDoList()
+        {
+            ItemsToDo = new List<Task>();
+        }
+        // XDocument class: https://learn.microsoft.com/en-us/dotnet/standard/linq/xdocument-class-overview
+
+        public void FindNotDoneItems()
+        {
+            var notDoneItems = ItemsToDo.Where(i =>!i.IsDone).ToList();
+            XDocument doc = new XDocument(
+                new XElement("Tasks",
+                    notDoneItems.Select(task => new XElement("Task",
+                        new XElement("Title", task.Title),
+                        new XElement("Description", task.Description),
+                        new XElement("DueDate", task.DueDate),
+                        new XElement("IsDone", task.IsDone)))));
+            doc.Save("NotDoneTasks.xml");
+        }
+
+        public void FindNextWeekItems()
+        {
+            var nextWeekItems = ItemsToDo.Where(i => i.DueDate >= DateTime.Now && i.DueDate <= DateTime.Now.AddDays(7)).ToList();
+            XDocument doc = new XDocument(
+                new XElement("Tasks",
+                    from task in nextWeekItems
+                    select new XElement("Task",
+                        new XElement("Description", task.Description))));
+            doc.Save("NextWeekTasks.xml");
+
+        }
+    }
+}
